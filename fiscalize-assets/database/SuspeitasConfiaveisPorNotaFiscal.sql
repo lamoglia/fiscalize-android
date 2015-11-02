225c71b1460be30e0508666f8038d126d5cb6565
Danilo Mendonça <favoratti@gmail.com>
2015-11-02 01:36:46 -0200

    Corrigindo SQL

--- fiscalize-assets/database/SuspeitasConfiaveisPorNotaFiscal.sql
+++ fiscalize-assets/database/SuspeitasConfiaveisPorNotaFiscal.sql
@@ -1,7 +1,7 @@
-SELECT suspeitas.notaFiscalId, suspeitas.soma as somaSuspeitas, confiaveis.soma as somaConfiaveis, suspeitas.soma+confiaveis.soma as somaFiscalizacoes, suspeitas.soma/confiaveis.soma as razaoConfiaveis
+SELECT suspeitas.notaFiscalId, suspeitas.soma as somaSuspeitas, confiaveis.soma as somaConfiaveis, suspeitas.soma+confiaveis.soma as somaFiscalizacoes, if(confiaveis.soma=0,"0",suspeitas.soma/confiaveis.soma) as razaoConfiaveis
 FROM
-(SELECT COUNT(*) as soma, notaFiscalId FROM Suspeita WHERE suspeita=false GROUP BY notaFiscalId) as suspeitas 
+(SELECT COUNT(*) as soma, notaFiscalId FROM Suspeita WHERE suspeita=true GROUP BY notaFiscalId) as suspeitas 
 LEFT OUTER JOIN
-(SELECT COUNT(*) as soma, notaFiscalId FROM Suspeita WHERE suspeita=true GROUP BY notaFiscalId) as confiaveis 
+(SELECT COUNT(*) as soma, notaFiscalId FROM Suspeita WHERE suspeita=false GROUP BY notaFiscalId) as confiaveis 
 ON suspeitas.notaFiscalId = confiaveis.notaFiscalId
 ORDER BY razaoConfiaveis DESC, somaFiscalizacoes DESC
